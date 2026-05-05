d1dab7b8a63e5d471cf2c46593ec1f256f2ec9ba
sojiidowu <sojiidowuayodeji@gmail.com>
2026-05-05 17:48:29 +0100

    Added basic try-except handling to prevent scraper crashes in Jumia scraper

--- scraper/jumia_scraper.py
+++ scraper/jumia_scraper.py
@@ -1,6 +1,7 @@
 from .selenium_engine import get_driver
 import time
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 def scrape_jumia_products(product_name):
@@ -18,12 +19,27 @@ def scrape_jumia_products(product_name):
     results = []
 
     for item in products:
-        name = item.find_element(By.CLASS_NAME, "name").text
-        price = item.find_element(By.CLASS_NAME, "prc").text
-        link = item.find_element(By.CSS_SELECTOR, "a.core").get_attribute("href")
-
-        img = item.find_element(By.CSS_SELECTOR, "div.img-c img")
-        image = img.get_attribute("data-src") or img.get_attribute("src")
+        # Product Name
+        try:
+            name = item.find_element(By.CLASS_NAME, "name").text
+        except:
+            name = "Unknown"
+        # Product Price
+        try:
+            price = item.find_element(By.CLASS_NAME, "prc").text
+        except:
+            price = "Unknown"
+        # Product Link
+        try:
+            link = item.find_element(By.CSS_SELECTOR, "a.core").get_attribute("href")
+        except:
+            link = "Unknown"
+        # Product Image
+        try:
+            img = item.find_element(By.CSS_SELECTOR, "div.img-c img")
+            image = img.get_attribute("data-src") or img.get_attribute("src")
+        except:
+            image = "Unknown"
 
         results.append({
             "name": name,
