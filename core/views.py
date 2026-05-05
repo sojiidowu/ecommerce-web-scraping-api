e29eac750c085b202f930364a32f4cfbe9fc6055
sojiidowu <sojiidowuayodeji@gmail.com>
2026-05-05 16:07:52 +0100

    Designed a view function to get product name from the frontend then run the scraper function and store it in a variable to pass it back to the frontend Configure the home url at the app level Created a dashboard(home to scrape and display products from Jumia

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from scraper.jumia_scraper import scrape_jumia_products
+
 
 # Create your views here.
+def home(request):
+    products = []
+    if request.method == "POST":
+        product_name = request.POST.get("product_name")
+
+        products = scrape_jumia_products(product_name)
+
+    return render(request, "core/home.html", {"products": products})
--- ecommerce_web_sraping/settings.py
+++ ecommerce_web_sraping/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ecommerce_web_sraping.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
