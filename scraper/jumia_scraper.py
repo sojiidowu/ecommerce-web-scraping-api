7b10e8dbae96cce5c7c5aa26d49450c026d83aa3
sojiidowu <sojiidowuayodeji@gmail.com>
2026-05-05 16:14:00 +0100

    Refined product link selector  using CSS selector a.core

--- scraper/jumia_scraper.py
+++ scraper/jumia_scraper.py
@@ -11,7 +11,7 @@ def scrape_jumia_products(product_name):
     driver.get(url)
 
     # wait for page to load
-    time.sleep(5)
+    time.sleep(20)
     # To get all product cards
     products = driver.find_elements(By.CLASS_NAME, "prd")
     print('Products found:', len(products))
@@ -20,9 +20,9 @@ def scrape_jumia_products(product_name):
     for item in products:
         name = item.find_element(By.CLASS_NAME, "name").text
         price = item.find_element(By.CLASS_NAME, "prc").text
-        link = item.find_element(By.TAG_NAME, "a").get_attribute("href")
+        link = item.find_element(By.CSS_SELECTOR, "a.core").get_attribute("href")
         image = item.find_element(By.TAG_NAME, "img").get_attribute("data-src")
-        
+
 
         results.append({
             "name": name,
