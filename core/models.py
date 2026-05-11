a718c5642e92f51142522c60a3e591c8bfa8028d
sojiidowu <sojiidowuayodeji@gmail.com>
2026-05-12 00:16:21 +0100

    Updated product model name, link and image to use TextField for long scraped URLs and product names

--- core/models.py
+++ core/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     # query = models.CharField(max_length=100) for what the user searched for
-    name = models.TextField(blank=True, null=True)
+    name = models.TextField()
     price = models.CharField(max_length=50)
-    link = models.URLField(unique=True)
-    image = models.URLField(blank=True, null=True)
+    link = models.TextField(unique=True)
+    image = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
