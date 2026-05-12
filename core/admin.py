cc6538384d28b11414993db5457e135d2ff56436
sojiidowu <sojiidowuayodeji@gmail.com>
2026-05-12 08:43:42 +0100

    Updated admin.py to include query in list, search, and filter

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,6 @@ from .models import Product
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'link', 'image', 'created_at')
-    search_fields = ('name', 'price')
-    list_filter = ('created_at',)
+    list_display = ('id', 'query', 'name', 'price', 'link', 'image', 'created_at')
+    search_fields = ('query', 'name', 'price')
+    list_filter = ('query', 'created_at',)
