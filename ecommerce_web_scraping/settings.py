bbd34e59131a07db7a94629594939ba8ed2f6f9b
sojiidowu <sojiidowuayodeji@gmail.com>
2026-05-14 17:48:37 +0100

    Setup Dockerized Django environment with PostgreSQL, Redis, and Celery

--- ecommerce_web_scraping/settings.py
+++ ecommerce_web_scraping/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.env')
+# load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'ecommerce_web_scraping.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+        'NAME': 'ecommerce_scraper_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Asidakasoj1',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
@@ -129,8 +129,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
