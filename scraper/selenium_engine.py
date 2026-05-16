4bdbe4e89a13cd979aaa4aac047bd568585f7625
sojiidowu <sojiidowuayodeji@gmail.com>
2026-05-16 19:29:50 +0100

    Updated Selenium engine to use built-in driver manager

--- scraper/selenium_engine.py
+++ scraper/selenium_engine.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from  selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -21,14 +19,8 @@ def get_driver():
     chrome_options.add_argument("--no-sandbox")
 
     chrome_options.add_argument("--disable-dev-shm-usage")
- 
-    chrome_options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115 Safari/537.36"
-    )
 
-    service = Service(ChromeDriverManager().install())
-
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
 
     driver.execute_script(
         "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
